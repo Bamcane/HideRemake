83571e650c472a4cef1eab75e6e340ede8230191
RemakePower <diandaokui@qq.com>
2023-08-13 11:26:15 +0800

    fix

--- src/game/variables.h
+++ src/game/variables.h
@@ -206,8 +206,8 @@ MACRO_CONFIG_INT(SvRescue, sv_rescue, 0, 0, 1, CFGFLAG_SERVER, "Allow /rescue co
 MACRO_CONFIG_INT(SvRescueDelay, sv_rescue_delay, 1, 0, 1000, CFGFLAG_SERVER, "Number of seconds between two rescues")
 MACRO_CONFIG_INT(SvPractice, sv_practice, 0, 0, 1, CFGFLAG_SERVER, "Enable practice mode for teams. Means you can use /rescue, but in turn your rank doesn't count.")
 
-MACRO_CONFIG_INT(SvNinjaRespawn, sv_ninja_respawn, 30, 0, 120, CFGFLAG_SERVER, "Ninja pickup be respawn time in seconds.")
-MACRO_CONFIG_INT(SvTimeLimit, sv_timelimit, 5, 0, 30, CFGFLAG_SERVER, "timelimit in mins.")
+MACRO_CONFIG_INT(SvNinjaRespawn, sv_ninja_respawn, 30, 0, 120, CFGFLAG_SERVER | CFGFLAG_GAME, "Ninja pickup be respawn time in seconds.")
+MACRO_CONFIG_INT(SvTimeLimit, sv_timelimit, 5, 0, 30, CFGFLAG_SERVER | CFGFLAG_GAME, "timelimit in mins.")
 
 MACRO_CONFIG_INT(ClVideoPauseWithDemo, cl_video_pausewithdemo, 1, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Pause video rendering when demo playing pause")
 MACRO_CONFIG_INT(ClVideoShowhud, cl_video_showhud, 0, 0, 1, CFGFLAG_CLIENT | CFGFLAG_SAVE, "Show ingame HUD when rendering video")
